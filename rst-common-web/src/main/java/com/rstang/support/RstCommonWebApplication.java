5aa93b5ae151c77e1e94206727501269660738aa
yeyx <yeyx@hyxt.com>
2017-08-30 14:40:23 +0800

    init github

--- rst-common-web/src/main/java/com/rstang/support/RstCommonWebApplication.java
+++ rst-common-web/src/main/java/com/rstang/support/RstCommonWebApplication.java
@@ -2,8 +2,10 @@ package com.rstang.support;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
+@PropertySource({"common-context.properties"})
 public class RstCommonWebApplication {
 
 	public static void main(String[] args) {
