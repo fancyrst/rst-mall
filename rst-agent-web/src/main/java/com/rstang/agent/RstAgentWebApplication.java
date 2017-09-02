d9c19670ebc5f2759e25b99ac046590e597bf906
fancyrst <202127762@qq.com>
2017-09-02 17:20:56 +0800

    init

--- rst-agent-web/src/main/java/com/rstang/agent/RstAgentWebApplication.java
+++ rst-agent-web/src/main/java/com/rstang/agent/RstAgentWebApplication.java
@@ -2,11 +2,14 @@ package com.rstang.agent;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
+@PropertySource({"agent-context.properties"})
 public class RstAgentWebApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(RstAgentWebApplication.class, args);
 	}
+
 }
