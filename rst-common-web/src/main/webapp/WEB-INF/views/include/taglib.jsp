9881abf0928efe41a7228b7936537c8ece970e11
yeyx <yeyx@hyxt.com>
2017-09-07 16:48:00 +0800

    init

--- rst-common-web/src/main/webapp/WEB-INF/views/include/taglib.jsp
+++
@@ -1,7 +0,0 @@
-<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
-<%@ taglib prefix="form" uri="http://www.springframework.org/tags/form" %>
-<%@ taglib prefix="fmt" uri="http://java.sun.com/jsp/jstl/fmt" %>
-<%@ taglib prefix="fn" uri="http://java.sun.com/jsp/jstl/functions" %>
-<%@ taglib prefix="fns" uri="/WEB-INF/tlds/fns.tld" %>
-<c:set var="ctx" value="${pageContext.request.contextPath}${fns:getAdminPath()}"/>
-<c:set var="ctxStatic" value="${pageContext.request.contextPath}/static"/>
--- rst-common-web/src/test/java/com/rstang/support/service/DictServiceTest.java
+++ rst-common-web/src/test/java/com/rstang/support/service/DictServiceTest.java
@@ -2,6 +2,7 @@ package com.rstang.support.service;
 
 import com.rstang.common.entity.User;
 import com.rstang.support.sys.entity.Dict;
+import com.rstang.util.key.IDCreator;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -26,6 +27,7 @@ public class DictServiceTest {
 		user.setId("u12345671");
 
 		Dict dict = new Dict();
+		dict.setId(IDCreator.getInstance().getID("sys.dict"));
 		dict.setLabel("归属公司");
 		dict.setType("11");
 		dict.setValue("123");
@@ -33,6 +35,7 @@ public class DictServiceTest {
 		dict.setDescription("归属DESC");
 		dict.setCreateBy(user);
 		dict.setUpdateBy(user);
+		dict.setIsNewRecord(true);//将setIsNewRecord(true)，手动传入ID：IDCreator.getInstance().getID("sys.dict")
 		dictService.save(dict);
 
 //		Assert.assertEquals(1, dictService.findList(new Dict()));
@@ -49,6 +52,20 @@ public class DictServiceTest {
 	
 	@Test
 	public void testUpdate() throws Exception {
+		User user = new User();
+		user.setId("u12345672");
+
+		Dict dict = new Dict();
+		dict.setId("S0217090716315601");
+		dict.setLabel("归属公司");
+		dict.setValue("321");
+		dict.setType("1");
+		dict.setSort(12);
+		dict.setDescription("归属DESC");
+		dict.setCreateBy(user);
+		dict.setUpdateBy(user);
+		//dict.setIsNewRecord(true); 更新时保证ID有值即可
+		dictService.save(dict);
 	}
 
 }
