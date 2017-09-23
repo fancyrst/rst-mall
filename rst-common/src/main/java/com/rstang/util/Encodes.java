1f799a95880877018cb40b36dadb8ffd5274fc11
yeyx <yeyx@hyxt.com>
2017-09-23 14:58:33 +0800

    init

--- rst-common/src/main/java/com/rstang/util/Encodes.java
+++ rst-common/src/main/java/com/rstang/util/Encodes.java
@@ -12,7 +12,6 @@ import org.apache.commons.codec.binary.Base64;
 import org.apache.commons.codec.binary.Hex;
 import org.apache.commons.lang3.StringEscapeUtils;
 
-import com.rstang.core.exception.ExceptionHandler;
 
 /**
  * 封装各种格式的编码解码工具类.
